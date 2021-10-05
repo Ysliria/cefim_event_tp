6bb82e6eb0a2e40c37958b3ac9b0f3710dfe289a
Mickaël AUGER <auger.mickael37@gmail.com>
2021-10-05 14:08:47 +0200

    [HOMEPAGE] Ajout des informations de l'entité Event

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(EventRepository $eventRepository): Response
     {
+        $events = $eventRepository->findBy([], ['startedAt' => 'ASC'], '3');
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'events' => $events,
         ]);
     }
 }
---
+++ src/Entity/Event.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EventRepository::class)
+ */
+class Event
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $startedAt;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $endedAt;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $placeNumber;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStartedAt(): ?\DateTimeImmutable
+    {
+        return $this->startedAt;
+    }
+
+    public function setStartedAt(\DateTimeImmutable $startedAt): self
+    {
+        $this->startedAt = $startedAt;
+
+        return $this;
+    }
+
+    public function getEndedAt(): ?\DateTimeImmutable
+    {
+        return $this->endedAt;
+    }
+
+    public function setEndedAt(\DateTimeImmutable $endedAt): self
+    {
+        $this->endedAt = $endedAt;
+
+        return $this;
+    }
+
+    public function getPlaceNumber(): ?int
+    {
+        return $this->placeNumber;
+    }
+
+    public function setPlaceNumber(int $placeNumber): self
+    {
+        $this->placeNumber = $placeNumber;
+
+        return $this;
+    }
+}
